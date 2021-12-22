fe45abab151e41f4c55034bfb04a2a085d671cb5
Austen Van Brogen <43625899+AustenVanBrogen@users.noreply.github.com>
2021-12-22 13:42:18 -0600

    moved the get request and handling to a function. Also changed it to update the month and year for the get request

--- NYTDiscordBot.js
+++ NYTDiscordBot.js
@@ -2,22 +2,31 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fs = require('fs');
 
 const http = new XMLHttpRequest();
+let date = new Date();
+let month = date.getMonth()+1; //1 indexing
+let year = date.getFullYear();
 //Read the file syncronously to avoid problems
 //Also, need to specify utf8 to get a string instead of a buffer of ascii integers
 let key = fs.readFileSync('keys/nytApiKey.txt', 'utf8'); 
-const apiURL = 'https://api.nytimes.com/svc/archive/v1/2021/12.json?api-key='+key
+const apiURL = `https://api.nytimes.com/svc/archive/v1/${year}/${month}.json?api-key=${key}`;
+console.log(date.getFullYear());
+access(apiURL);
 
-http.open("GET", apiURL);
-http.send();
 
-http.onload=(e)=>{
-    //Makes the JSON string human-readable
-    let resText = JSON.stringify(JSON.parse(http.responseText), null, 4);
-    //console.log(resText);
-    fs.writeFile("NYTResponse.txt", resText, (error) =>{
-        if(error)
-        {
-            console.log(error);
-        }
-    });
+function access(url)
+{
+    http.open("GET", url);
+    http.send();
+
+    http.onload=(e)=>{
+        //Makes the JSON string human-readable
+        let resText = JSON.stringify(JSON.parse(http.responseText), null, 4);
+        //console.log(resText);
+        fs.writeFile("NYTResponse.txt", resText, (error) =>{
+            if(error)
+            {
+                console.log(error);
+            }
+        });
+    }
 }
