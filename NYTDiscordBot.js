d3c0a69817b83e81d44bd5b28e359c7d33f0da28
Austen Van Brogen <43625899+AustenVanBrogen@users.noreply.github.com>
2021-12-31 00:03:58 -0600

    Made the api calls syncronous and made the program work

--- NYTDiscordBot.js
+++ NYTDiscordBot.js
@@ -1,5 +1,6 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fs = require('fs');
+const async = require('async');
 
 const http = new XMLHttpRequest();
 let date = new Date();
@@ -12,68 +13,74 @@ let secondsToWait = 30;
 //Read the file syncronously to avoid problems
 //Also, need to specify utf8 to get a string instead of a buffer of ascii integers
 
-//access(apiURL);
-timerFunction();
-function timerFunction()
-{
-    access(apiURL);
-    setTimeout(() =>{
-        timerFunction();
-    },secondsToWait * millisecondsPerSecond);
-}
-
-
-function access(url)
+const access = async function(url)
 {
     let article;
     let articleTitle;
     let articleURL;
-    let mostRecentArticleTitleOnEntry = mostRecentArticleTitle;
     let month = date.getMonth()+1; //1 indexing
     let year = date.getFullYear();
     apiURL = `https://api.nytimes.com/svc/archive/v1/${year}/${month}.json?api-key=${key}`;
 
-    http.open("GET", apiURL);
+    http.open("GET", apiURL, false);
 
     http.onload=(e)=>{
-        try
+        //Makes the JSON string human-readable
+        let resJson;
+        try{
+            resJson = JSON.parse(http.responseText);
+        }
+        catch(error)
         {
-            //Makes the JSON string human-readable
-            let resJson = JSON.parse(http.responseText);
+            console.log("The response wasn't a json");
+            console.log(http.responseText);
+            return;
+        }
 
-            //Saves the response to a file called "NYTResponse.txt"
-            // let resText = JSON.stringify(JSON.parse(http.responseText), null, 4);
-            // fs.writeFile("NYTResponse.txt", resText, (error) =>{
-            //     if(error)
-            //     {
-            //         console.log(error);
-            //     }
-            // });
+        //Saves the response to a file called "NYTResponse.txt"
+        // let resText = JSON.stringify(JSON.parse(http.responseText), null, 4);
+        // fs.writeFile("NYTResponse.txt", resText, (error) =>{
+        //     if(error)
+        //     {
+        //         console.log(error);
+        //     }
+        // });
 
-            article = resJson.response.docs[resJson.response.docs.length-1];
-            articleURL = article.web_url;
-            articleTitle = article.headline.main;
-            
-            if(mostRecentArticleTitle == null && articleTitle != null)
-            {
-                console.log(`Set initial value: ${articleTitle}`);
-                mostRecentArticleTitle = articleTitle;
-            }
-            else if(articleTitle !== mostRecentArticleTitle && articleTitle === mostRecentArticleTitleOnEntry)
-            {
-                mostRecentArticleTitle = articleTitle;
-                console.log('The New York Times have done it again!');
-                console.log(articleTitle);
-                console.log(articleURL);
-            }
-            else{
+        article = resJson.response.docs[resJson.response.docs.length-1];
+        articleURL = article.web_url;
+        articleTitle = article.headline.main;
+        
+        if(mostRecentArticleTitle == null && articleTitle != null)
+        {
+            console.log(`Set initial value: ${articleTitle}`);
+            mostRecentArticleTitle = articleTitle;
+        }
+        else if(articleTitle !== mostRecentArticleTitle)
+        {
+            mostRecentArticleTitle = articleTitle;
+            console.log('The New York Times have done it again!');
+            console.log(articleTitle);
+            console.log(articleURL);
+        }
+        else{
 
-            }
+        }
+    }
+    http.send();
+}
+
+const timerFunction = async function()
+{
+    await access(apiURL);
+    setTimeout(() =>{
+        try{
+        timerFunction();
         }
         catch(error)
         {
             console.log("There was an error");
         }
-    }
-    http.send();
+    },secondsToWait * millisecondsPerSecond);
 }
+
+timerFunction();
